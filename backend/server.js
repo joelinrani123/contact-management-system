2ad5240dfa41b8292bb0738ded73373be8e4e14f
JOELIN RANI J <joelinrani44@gmail.com>
2025-12-04 12:10:59 +0530

    changes for backend deployment

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,11 @@ import contactRoutes from './routes/contactRoutes.js';
 
 dotenv.config();
 const app=express();
-const PORT=5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.use(cors({
+    
     origin: "http://localhost:5173", 
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
